fc6f5f7406b8ff8ac0298618af00dd8331be67b5
Ridwan Aji Bari <ajibari@gmail.com>
2018-03-22 22:54:13 +0700

    add gunmen answer

---
+++ algorithm_gunmen.php
@@ -0,0 +1,115 @@
+<?php
+
+function maximumGunmen($arr, $wall){
+	$gunmen = 0;
+
+	$gunmenPos = [];
+	$vertical = [];
+	$horizontal = [];
+	$element = null;
+	$x = 1;
+
+	// cari most wall tapi tidak boleh max sesuai count
+	$a = 0;
+	$mostWall = null;
+	foreach ($arr as $k => $v) {
+		$counts = array_count_values($arr[$a]);
+		if($counts[$wall] < count($arr[$a])){
+			$mostWall = $counts[$wall];	
+			break;
+		}
+		$a++;	
+	}
+	
+	$totalMostWall = 0;
+
+	foreach($arr as $k => $v){
+
+		$i = 1;
+		
+		foreach($v as $k2 => $v2){
+
+			if($v2 != $wall){
+				
+				if($vertical[$i] != 'open' || !$vertical[$i]){
+					
+					if($horizontal[$x] != 'open'){
+						array_push($gunmenPos, [$x, $i]);
+						$vertical[$i] = 'open';
+						$horizontal[$x] = 'open';
+					}
+
+				}
+
+
+			}
+			else if($v2 == $wall){
+				$vertical[$i] = 'closed';
+			}
+
+			if($element != $v2){
+				$horizontal[$x] = 'closed';
+			}
+
+			$element = $v2;
+
+			$i++;
+		}
+
+		//hitung wall terbanyak
+			$dataArr = array_count_values($v);
+			$countWall = $dataArr[$wall];
+
+			if($countWall == $mostWall){
+				$totalMostWall++;
+			}
+
+		$x++;
+	}
+
+	$data['result'] = $gunmenPos;
+	$data['most_wall'] = $totalMostWall;
+	return $data;
+}
+
+function placeGunmen($arr){
+
+}
+
+function sortWall($arr, $wall){
+	$wallTotal = [];
+	foreach($arr as $k => $v){
+		$counts = array_count_values($v);
+		array_push($wallTotal, $counts[$wall]);
+	}
+
+	array_multisort($wallTotal, SORT_DESC, $arr, SORT_DESC);
+	return $arr;
+}
+
+$data = [
+	['x', 'x', 'x', '_'],
+	['_', '_', '_', '_'],
+	['_', 'x', '_', '_'],
+	['x', 'x', 'x', '_'],
+	['x', 'x', 'x', 'x']
+];
+
+$f = sortWall($data, 'x');
+
+
+$countGunMen = maximumGunmen($f, 'x');
+$totalGunMen = count($countGunMen['result']);
+$totalGunMenPlacement = $countGunMen['most_wall'];
+echo '<pre>';
+var_dump( $countGunMen['result'] );
+echo '</pre>';
+
+echo '<hr/>';
+echo $countGunMen['most_wall'];
+
+echo '<hr/>';
+echo 'total banyak gunmen = '.$totalGunMen;
+echo '<br/>';
+echo 'total banyak placemen gunmen = '.$totalGunMenPlacement;
+?>
